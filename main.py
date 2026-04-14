4ec926e3f355132bae3e43b24e2c7fea01a6314e
Egor-milk <mrpixelegor@gmail.com>
2026-04-14 14:10:29 +0700

    начал работу по созданию таск трекера

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-#init
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+app = FastAPI()
+
+class Task(BaseModel): #создаёт классы с жёсткой валидацией данных
+    name: str
+    description: str | None = None
+
+
+
+@app.get('/tasks')
+def get_tasks():
+    task = Task(name='Запиши это видео')
+    return {'data': task}
+
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host='localhost', port=8000, reload=True)
